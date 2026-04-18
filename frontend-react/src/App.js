2e3beae39d2580348771c6d65e7c5b37cdb12ed5
williamsonnet <william.sonnet2012@gmail.com>
2026-04-18 09:17:21 +0000

    Première page React World Cup Bet

---
+++ frontend-react/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <header className="App-header">
+    <h1>⚽ World Cup Bet 2026</h1>
+    <p>Bienvenue sur ton app de pronostics !</p>
+    <p>Bientôt tu pourras parier sur tes équipes préférées 🏆</p>
+  </header>
+</div>
+    
+  );
+}
+
+export default App;
